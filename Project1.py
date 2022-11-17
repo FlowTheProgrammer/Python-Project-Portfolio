266ce5dd93f5615ad1e09d9f78391fbe1bef74df
Anthony W <anthonywashington2104@gmail.com>
2022-11-16 20:17:13 -0500

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -10,15 +10,22 @@ count = 0
 while count < howMany:
     studentName = input("Enter a student name.")
     studentGrade = int(input("Enter a grade for the student?"))
+    print()
     myStudents[studentName] = studentGrade
     count += 1
 
 def printMenu():
+    print()
     print("List of Students")
     print("Change a student's grade")
     print("Add/Remove a student")
-    print("")
-    print("")
+    print()
+
+def executeMenu(x):
+    if x == "1":
+        printStudents()
+    if x == "2":
+        changeGrade()
 
 def getLetterGrade(x):
     if x >= 97:
@@ -56,5 +63,36 @@ def printStudents():
     for key, values in myStudents.items():
         print("Student Name:", key, "     Student Grade:", values, "     Student Letter Grade:", getLetterGrade(values))
 
-printStudents()
+def changeGrade():
+    print()
+    print("Change A Students Grade")
+    print("----------------------")
+    print()
+    changeName = input("Enter a student's name:")
+
+    found = False
+    for key, values in myStudents.items():
+        if changeName == key:
+            new_grade = print("What is the new grade for", changeName + "?")
+            myStudents[key] = new_grade
+            found = True
+            break
+    if found == False:
+        print("Name not Found")
+
+
+
+printMenu()
+userOption = input("Choose an option:")
+while userOption != "Quit"  and userOption != "quit":
+    if userOption != "1" and userOption != "2" and userOption != "3":
+        print("Invalid Option")
+        userOption = input("Choose an option:")
+    else:
+        executeMenu(userOption)
+        printMenu()
+        userOption = input("Choose an option:")
+
+
+
 
