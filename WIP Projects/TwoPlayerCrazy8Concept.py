603b7a23d1525fc23b0a511ca22bc7dd6f0ebca4
Anthony W <59625958+FlowTheProgrammer@users.noreply.github.com>
2023-01-03 22:13:06 -0500

    Update TwoPlayerCrazy8Concept.py

--- WIP Projects/TwoPlayerCrazy8Concept.py
+++ WIP Projects/TwoPlayerCrazy8Concept.py
@@ -34,18 +34,22 @@ class Deck():
     
     def deal_one_card(self):
         return self.allCards.pop()
+    
+    def reshuffle(self,card):
+        self.allCards.append(card)
 
 #Hand Class - Stores Card Classes
 class Hand:
     
     def __init__(self):
         self.cards = []
-        self.value = 0
 
         #Adds Card to "Hand"
     def add_card(self,card):
         self.cards.append(card)
-        self.value += values[card.rank]
+
+    def place_card(self,loc):
+        return self.cards.pop(loc)
 
 class Pile:
     
@@ -55,7 +59,11 @@ class Pile:
         #Adds Card to Pile
     def add_card(self,card):
         self.cards.append(card)
+
+    def return_one_card(self):
+        return self.cards.pop(0)
      
+#Displays Player 1's view (# of Opp. Cards, card in play and cards in hand) (Functional)
 def player_one_view(player1,player2,pile):
     print(f"Player 2 has {len(player2.cards)} cards\n")
     print(f"The card in play is {str(pile.cards[-1])}")
@@ -65,6 +73,7 @@ def player_one_view(player1,player2,pile):
         hand.append(str(card))
     print(hand)
 
+#Displays Player 2's view (# of Opp. Cards, card in play and cards in hand) (Functional)
 def player_two_view(player2,player1,pile):
     print(f"Player 1 has {len(player1.cards)} cards\n")
     print(f"The card in play is {str(pile.cards[-1])}")
@@ -74,6 +83,7 @@ def player_two_view(player2,player1,pile):
         hand.append(str(card))
     print(hand)
 
+#Checks to see if either player has won (Haven't checked)
 def checkWin(player1,player2):
     if player1.cards == []:
         return False
@@ -82,29 +92,76 @@ def checkWin(player1,player2):
     else:
         return True
 
-#Might edit to have drawing and playing be two different functions
-def choice(player,pile,deck):
+#Function that allows the player to either choose to play or draw (Functional)
+def choice(player,pile,deck): 
     while True:
         user_input = input("Would you like to play a card or draw. Input 'draw' or 'play'")
         if user_input.lower() == "play":
             os.system('cls')
-            play_or_draw(user_input.lower())
+            player_play(player,pile,deck)
             break
         elif user_input.lower() == "draw":
             os.system('cls')
-            play_or_draw(user_input.lower())
+            player_draw(player,pile,deck)
             break
         else:
             print("I don't understand your input, Try Again.\n")
-            #Maybe edit this so that it clears everything and reprints game
-
-def play_or_draw(x):
-    pass
-
-def check_deck_count():
-    pass
+            
 
 
+
+#Function to allow the player to play a card (Functional)
+def player_play(player,pile,deck):
+    playable_cards = []
+    for i in player.cards:
+        if i.rank == pile.cards[-1].rank or i.suit == pile.cards[-1].suit:
+            playable_cards.append(str(i))
+    if playable_cards == []:
+        print("No cards in your hand are playable, proceeding to draw a card.")
+        null = input("\nPress Enter to Confirm.")
+        os.system('cls')
+        player_draw(player,pile,deck)
+    else:
+        print(playable_cards)
+        while True:
+            user_input = input("Type the name of the card you would like to play or type draw to draw instead. (Case-Sensative)")
+            if user_input in playable_cards:
+                for count, i in enumerate(player.cards):
+                    if str(i) == user_input:
+                        pile.add_card(player.place_card(count))
+                break
+            elif user_input.lower == "draw":
+                print("\n Drawing a card instead")
+                null = input("\nPress Enter to Confirm.")
+                os.system('cls')
+                player_draw(player,pile,deck)
+                break
+            else:
+               print("I don't understand your input, Try Again.\n")
+    null = input("Press Enter to end your turn")
+
+
+
+#Function to allow a player to draw a card (Functional)
+def player_draw(player,pile,deck):
+    player.add_card(deck.deal_one_card())
+    print("You new hand is: ")
+    hand = []
+    for card in player.cards:
+        hand.append(str(card))
+    print(hand)
+    null = input("\n Press Enter when you want to end your turn.")
+
+#Reshuffles deck from pile if out of cards (Haven't checked)
+def check_deck_count(deck,pile):
+    if deck.allCards == 0:
+        for i in len(pile.cards) - 1:
+            deck.reshuffle(pile.return_one_card)
+    else:
+        pass
+
+
+#Init
 while True:
     print("Welcome to Crazy Eights")
     newDeck = Deck()
@@ -118,6 +175,7 @@ while True:
     discard_pile.add_card(newDeck.deal_one_card())
     break
 
+
 Playing = True
 while Playing:
     os.system('cls')
@@ -126,7 +184,7 @@ while Playing:
     player_one_view(player_one_hand,player_two_hand,discard_pile)
     choice(player_one_hand,discard_pile,newDeck)
     Playing = checkWin(player_one_hand,player_two_hand)
-    #checkDeckCount
+    check_deck_count(newDeck,discard_pile)
     if Playing:
         os.system('cls')
         null = input("Player 2's turn! Press Enter to continue")
@@ -134,5 +192,5 @@ while Playing:
         player_two_view(player_two_hand,player_one_hand,discard_pile)
         choice(player_two_hand,discard_pile,newDeck)
         Playing = checkWin(player_one_hand,player_two_hand)
-        check_deck_count()
-    break
+        check_deck_count(newDeck,discard_pile)
+    break #Remove when done
