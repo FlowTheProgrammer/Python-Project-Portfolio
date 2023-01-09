bf23e9000fe902820ba307863c2300d4127760ca
Anthony W <59625958+FlowTheProgrammer@users.noreply.github.com>
2023-01-08 20:42:45 -0500

    Create 7. Hangman.py

---
+++ 100 Days of Code Projects/Beginner Projects/7. Hangman.py
@@ -0,0 +1,44 @@
+"""
+Hangman game
+Loops/List/If-statements
+
+Used some advanced function for simplicity
+"""
+from random_word import RandomWords
+
+r = RandomWords()
+
+randomWord = r.get_random_word()
+
+wordList = []
+
+for i in range(len(randomWord)):
+    wordList.append("_")
+
+Playing = True
+Lives = 6
+
+print("Welcome to Hangman!")
+
+while Playing:
+    guess_letter = input("\nGuess a letter: ").lower()
+
+    if guess_letter in wordList:
+        print("You already guessed that letter!\n")
+
+    for i,letter in enumerate(randomWord):
+        if guess_letter == letter:
+            wordList[i] = guess_letter
+
+    if guess_letter not in randomWord:
+        Lives -= 1
+        print(f"\nLetter not in word. Lives:{Lives}\n")
+        if Lives == 0:
+            Playing = False
+            print("You Lost!")
+            print(f'\nThe word was {randomWord}')
+
+    print(wordList)
+    if "_" not in wordList:
+        Playing = False
+        print("You have won!")
