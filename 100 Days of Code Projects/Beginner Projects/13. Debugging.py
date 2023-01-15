6c4f082fffc421406849417c5676f97a05ee8075
Anthony W <59625958+FlowTheProgrammer@users.noreply.github.com>
2023-01-14 20:24:43 -0500

    Create 13. Debugging.py

---
+++ 100 Days of Code Projects/Beginner Projects/13. Debugging.py
@@ -0,0 +1,15 @@
+"""
+There was no project for this day, just debugging code.
+
+Here's Fizzbuzz!
+"""
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
