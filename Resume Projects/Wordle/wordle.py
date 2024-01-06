c30e43d25dd8536419c3b2cf5fabeca0815168de
Anthony W <59625958+FlowTheProgrammer@users.noreply.github.com>
2024-01-06 09:23:42 -0500

    Finished Wordle
    -Switch to Finished Projects
    -Bugs Fixed
    -Comments Added
    
    (File Pathing may have issues when downloaded)

--- Resume Projects/Wordle/wordle.py
+++ Resume Projects/Wordle/wordle.py
@@ -8,12 +8,6 @@ WRONG_COLOR = '#403c3c'
 IN_WORD_COLOR = '#b89c3c'
 RIGHT_COLOR = '#588c4c'
 
-#List of letters
-alphabet = ['q', 'w', 'e', 'r', 't', 'y', 
-            'u', 'i','o', 'p', 'a', 's', 
-            'd', 'f', 'g', 'h', 'j', 'k', 
-            'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
-
 #Keyboard Layout
 qwerty_layout = [
     'QWERTYUIOP',
@@ -21,6 +15,7 @@ qwerty_layout = [
     'ZXCVBNM'
 ]
 
+#Keybaord Dictionary (QWERTY layout)
 keyboard_dict = {
     'q': 0, 'w': 1, 'e': 2, 'r': 3, 't': 4, 'y': 5, 'u': 6, 'i': 7, 'o': 8, 'p': 9,
     'a': 10, 's': 11, 'd': 12, 'f': 13, 'g': 14, 'h': 15, 'j': 16, 'k': 17, 'l': 18,
@@ -35,35 +30,36 @@ class GameBoard(tk.Tk):
         self.geometry("1920x1080")
 
         #Variables
-        self.boxes = []
-        self.buttons = []
+        self.boxes = [] #List of answer boxes
+        self.buttons = [] #List of keyboard
         self.current_row = 0
         self.current_col = 0
         self.current_letters = 0
         self.word = ''
-        self.cpy_word = self.word
+        self.cpy_word = self.word #Copy of word
         self.answer = self.get_random_word()
-        self.check_answer = self.answer
-        self.game_end = False
-        print(self.answer)
+        self.check_answer = self.answer #Copy of answer
+        self.game_end = False #
+
+        #Used to see answer (Can comment out)
+        #print(self.answer)
 
 
 
         #Background color for game
-        background_color = tk.Frame(self, bg=BG_COLOR, padx=10, pady=10)
-        background_color.pack(fill=tk.BOTH, expand=True)
+        game_background = tk.Frame(self, bg=BG_COLOR, padx=10, pady=10)
+        game_background.pack(fill=tk.BOTH, expand=True)
 
         #Title Creation
-        self.title = tk.Label(background_color, text="WORDLE", font=("comic-sans", 16, "bold"), fg = 'white', bg =BG_COLOR )
+        self.title = tk.Label(game_background, text="WORDLE", font=("comic-sans", 16, "bold"), fg = 'white', bg =BG_COLOR )
         self.title.pack(pady=100)
 
-         #Text Box Layout
-
+        #Text Box Layout
         for i in range(6):
-            frame = tk.Frame(background_color, bg='black')
+            frame = tk.Frame(game_background, bg='black')
             frame.pack(side=tk.TOP, pady=1)
 
-            box_in_row = []
+            box_in_row = [] #Used to keep draw of boxes in a row (for 2D array)
 
             for j in range(5):
                 box = tk.Text(frame, width=8, height = 4, 
@@ -71,11 +67,11 @@ class GameBoard(tk.Tk):
                 box.grid(row=i, column=j, padx=1, pady=1)
                 box_in_row.append(box)
                 
-            self.boxes.append(box_in_row)
+            self.boxes.append(box_in_row) #2D array to easily edit boxes
 
         #Keyboard Layout 
         for row in qwerty_layout:
-            frame = tk.Frame(background_color, bg = BG_COLOR)
+            frame = tk.Frame(game_background, bg = BG_COLOR)
             frame.pack(side=tk.TOP, pady=5)
     
             for letter in row:
@@ -83,11 +79,11 @@ class GameBoard(tk.Tk):
                                    command=lambda l=letter: self.add_Letter(l) ,
                                    bg='grey', fg = 'white', font = ('bold'))
                 button.pack(side=tk.LEFT, padx=2)
-                self.buttons.append(button)
+                self.buttons.append(button) #List to easily track and edit buttons
         
 
         #Frame for Backspace and Enter
-        frame = tk.Frame(background_color, bg = BG_COLOR)
+        frame = tk.Frame(game_background, bg = BG_COLOR)
         frame.pack(side=tk.TOP, pady=1)
 
         backspace_button = tk.Button(frame, text="Backspace", width=10, height=2, bg='grey', fg = 'white', font = ('bold'), command=self.del_letter)
@@ -126,16 +122,19 @@ class GameBoard(tk.Tk):
                 self.boxes[self.current_row][self.current_col].delete(1.0, tk.END)
 
     def get_random_word(self):
-        with open('WIP Projects\Wordle/word-list.txt', 'r') as file:
+        """Returns a random word from answers.txt"""
+        with open('Resume Projects\Wordle/answers.txt', 'r') as file:
                 content = file.read().split()
                 return random.choice(content)
 
     def in_word_list(self):
-        with open('WIP Projects\Wordle\word-list.txt', 'r') as file:
+        """Returns if word is in word list"""
+        with open('Resume Projects\Wordle\word-list.txt', 'r') as file:
                 content = file.read()
                 return self.word.lower() in content
 
     def submit_answer(self):
+        """Game logic based on sumbitted answer"""
 
         if not self.game_end:
             #Checks to see if word is unfinished
@@ -144,7 +143,7 @@ class GameBoard(tk.Tk):
 
             #Checks if the  user guessed the right word
             elif self.word.lower() == self.answer.lower():
-                self.change_box_layout()
+                self.change_box_layout() #Changes colors of board and keyboard 
                 messagebox.showerror('Winner!', 'You Won!')
                 self.game_end = True
 
@@ -160,53 +159,65 @@ class GameBoard(tk.Tk):
                 messagebox.showerror('Sorry!', 'Word not in word list')
 
     def change_box_layout(self):
+        """Changes color of keyboard and gameboard based on guessed word"""
+        #Copys word for editing
         self.cpy_word = self.word
+
+        #Logic to prioritize letters that are in the correct positions first to avoid errors
         for index, letter in enumerate(self.word.lower()):
             if self.word.lower()[index] == self.check_answer[index]:
-                    self.buttons[keyboard_dict[letter]].config(bg=RIGHT_COLOR)
-                    self.check_answer = self.check_answer[:index] + '?' + self.check_answer[index + 1:]
-                    self.cpy_word = self.cpy_word[:index] + '!' + self.cpy_word[index + 1:]
-                    self.boxes[self.current_row][index].config(bg=RIGHT_COLOR)
+                    self.buttons[keyboard_dict[letter]].config(bg=RIGHT_COLOR) #Changes respective keyboard key to green
+                    self.check_answer = self.check_answer[:index] + '?' + self.check_answer[index + 1:] #Logic to deal more than 1 instance of a letter in a word
+                    self.cpy_word = self.cpy_word[:index] + '!' + self.cpy_word[index + 1:] #Logic to deal more than 1 instance of a letter in a word
+                    self.boxes[self.current_row][index].config(bg=RIGHT_COLOR) #Changes gameboard space to green
 
 
-        for index, letter in enumerate(self.word.lower()):
+        #Loops through each letter in the guessed word       
+        for index, letter in enumerate(self.word.lower()): 
             pos = self.where_in_position(index)
+
+            #Changes keyboard key to grey if not found in word
             if pos == 'none':
                 if self.boxes[self.current_row][index].cget("bg") == RIGHT_COLOR:
                     pass
                 else:
                     self.boxes[self.current_row][index].config(bg=WRONG_COLOR)
 
+            #Changes keyboard key to yellow if found in word (but not in the right place)
             elif pos == 'in':
                 self.boxes[self.current_row][index].config(bg=IN_WORD_COLOR)
 
-            elif pos == 'correct':
-                self.boxes[self.current_row][index].config(bg=RIGHT_COLOR)
-
-
+        #Resets copy of word and answer
         self.check_answer = self.answer
         self.cpy_word = self.word
 
 
     def where_in_position(self,index):
-        print(self.check_answer)
-        if self.cpy_word.lower()[index] not in self.check_answer:
-            if self.cpy_word.lower()[index] != "!":
-                self.buttons[keyboard_dict[self.cpy_word.lower()[index]]].config(bg = WRONG_COLOR)
+        """Returns where the letter is located in the word"""
+
+        if self.cpy_word.lower()[index] not in self.check_answer: #Chekcs if letter is no in the word
+            if self.cpy_word.lower()[index] != "!": #Skips over ! to avoid errors
+                if self.buttons[keyboard_dict[self.cpy_word.lower()[index]]].cget('bg') != RIGHT_COLOR: #Checks if letter has already been confirmed
+                    if self.buttons[keyboard_dict[self.cpy_word.lower()[index]]].cget('bg') != IN_WORD_COLOR: #Checks if letter has already been confirmed
+                        self.buttons[keyboard_dict[self.cpy_word.lower()[index]]].config(bg = WRONG_COLOR) #Changes button to "Grey"
+
             return "none"
         
-        elif self.cpy_word.lower()[index] in self.check_answer:
-            self.check_answer = self.check_answer.replace(self.cpy_word.lower()[index],'?',1)
-            self.buttons[keyboard_dict[self.cpy_word.lower()[index]]].config(bg = IN_WORD_COLOR)
+        elif self.cpy_word.lower()[index] in self.check_answer: #Chekcs if letter is in the word
+            self.check_answer = self.check_answer.replace(self.cpy_word.lower()[index],'?',1) #Replaces the letter to handle ducplicate letters
+            if self.buttons[keyboard_dict[self.cpy_word.lower()[index]]].cget('bg') != RIGHT_COLOR: #Checks if letter has already been confirmed
+                self.buttons[keyboard_dict[self.cpy_word.lower()[index]]].config(bg = IN_WORD_COLOR) #Changes button to yellow
+
             return 'in'
         
 
     def checkLoss(self):
+        """Function to check if a player has lost the game"""
+        #Checks if the current row is beyond the gameboard, which indicates a loss
         if self.current_row >= 6:
             messagebox.showerror('Oops!', f'You lost! The answer was {self.answer}')
             self.game_end = True
-            
-        
+               
 
 #Game Creation
 myBoard = GameBoard()
